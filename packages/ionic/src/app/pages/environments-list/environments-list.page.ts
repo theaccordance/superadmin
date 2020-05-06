017aa2fd4deee811e90146ff6dc98127b945f800
Joe Mainwaring <joseph.mainwaring@gmail.com>
2020-05-06 09:21:49 -0500

    add environments-list page module

---
+++ packages/ionic/src/app/pages/environments-list/environments-list.page.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-environments-list",
+  templateUrl: "./environments-list.page.html",
+  styleUrls: ["./environments-list.page.scss"],
+})
+export class EnvironmentsListPage implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
