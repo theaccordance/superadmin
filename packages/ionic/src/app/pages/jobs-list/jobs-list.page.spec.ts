46944d7dff4a5b31372ee5b3b3faff1752f90037
Joe Mainwaring <joseph.mainwaring@gmail.com>
2020-05-06 09:23:25 -0500

    add jobs-list page module

---
+++ packages/ionic/src/app/pages/jobs-list/jobs-list.page.spec.ts
@@ -0,0 +1,24 @@
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { IonicModule } from "@ionic/angular";
+
+import { JobsListPage } from "./jobs-list.page";
+
+describe("JobsListPage", () => {
+  let component: JobsListPage;
+  let fixture: ComponentFixture<JobsListPage>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [JobsListPage],
+      imports: [IonicModule.forRoot()],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(JobsListPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it("should create", () => {
+    expect(component).toBeTruthy();
+  });
+});
